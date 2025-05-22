ccf0663893431c855e6fc9dfd71c9cafba8c1ea6
ZakariaR2000 <zakariarjoub14@gmail.com>
2025-05-22 18:52:20 +0300

    Manually moved wwwroot inside LaptopShop

---
+++ ApiControllers/ItemsController.cs
@@ -0,0 +1,115 @@
+﻿using LapShop.Bl;
+using LapShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LapShop.ApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        IItem oItem;
+        public ItemsController(IItem iitem)
+        {
+            oItem = iitem;
+        }
+
+        // GET: api/<ItemsController>
+
+        /// <summary>
+        /// get all items form database
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ApiResponse Get()
+        {
+            ApiResponse oApiResponse = new ApiResponse();
+            oApiResponse.Data = oItem.GetAll();
+            oApiResponse.Errors = null;
+            oApiResponse.StatusCode = "200";
+
+            return oApiResponse;
+        }
+
+        // GET api/<ItemsController>/5
+        /// <summary>
+        /// get item by id
+        /// </summary>
+        /// <param name="id">Item id</param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public ApiResponse Get(int id)
+        {
+            ApiResponse oApiResponse = new ApiResponse();
+            oApiResponse.Data = oItem.GetById(id);
+            oApiResponse.Errors = null;
+            oApiResponse.StatusCode = "200";
+
+            return oApiResponse;
+        }
+
+        //[HttpGet("{id}")]
+        //public IActionResult Get(int id)
+        //{
+        //    ApiResponse oApiResponse = new ApiResponse();
+        //    oApiResponse.Data = oItem.GetById(id);
+        //    oApiResponse.Errors = null;
+        //    oApiResponse.StatusCode = "200";
+
+        //    return BadRequest(new object() { });
+        //}
+
+        // GET api/<ItemsController>/5
+        [HttpGet("GetByCategoryId/{categoryId}")]
+        public ApiResponse GetByCategoryId(int categoryId)
+        {
+
+            ApiResponse oApiResponse = new ApiResponse();
+            oApiResponse.Data = oItem.GetAllItemsData(categoryId);
+            oApiResponse.Errors = null;
+            oApiResponse.StatusCode = "200";
+
+            return oApiResponse;
+        }
+
+        // POST api/<ItemsController>
+        [HttpPost]
+        public ApiResponse Post([FromBody] TbItem item)
+        {
+
+            try
+            {
+                ApiResponse oApiResponse = new ApiResponse();
+
+                oItem.Save(item);
+                oApiResponse.Data = "done";
+                oApiResponse.Errors = null;
+                oApiResponse.StatusCode = "200";
+                return oApiResponse;
+
+            }
+            catch (Exception ex)
+            {
+                ApiResponse oApiResponse = new ApiResponse();
+
+                oApiResponse.Data = null;
+                oApiResponse.Errors = ex.Message;
+                oApiResponse.StatusCode = "502";
+                return oApiResponse;
+
+            }
+
+        }
+
+        [HttpPost]
+        [Route("Delete")]
+        public void Delete([FromBody] int id)
+        {
+            oItem.Delete(id);
+        }
+
+        
+    }
+}
---
+++ Areas/admin/Controllers/CategoriesController.cs
@@ -0,0 +1,68 @@
+﻿using LapShop.Bl;
+using LapShop.Models;
+using LapShop.Utlities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace LapShop.Areas.admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Area("admin")]
+    public class CategoriesController : Controller
+    {
+        public CategoriesController(ICategories category)
+        {
+            oClsCategories = category;
+        }
+        ICategories oClsCategories;
+
+        public IActionResult List()
+        {
+
+            return View(oClsCategories.GetAll());
+        }
+
+        public IActionResult Edit(int? categoryId)
+        {
+            var category = new TbCategory();
+
+            if(categoryId != null)
+            {
+                category = oClsCategories.GetById(Convert.ToInt32(categoryId));
+            }
+            
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Save(TbCategory category , List<IFormFile> Files)
+        {
+            if (!ModelState.IsValid)
+                return View("Edit" , category);
+
+            category.ImageName = await Helper.UploadImage(Files , "Categories");
+
+
+            oClsCategories.Save(category);
+
+            return RedirectToAction("List");
+
+        }
+
+        public IActionResult Delete(int categoryId)
+        {
+
+            oClsCategories.Delete( categoryId);
+
+            return RedirectToAction("List");
+
+        }
+
+
+        //I think that it's method Wrong to be here
+
+
+    }
+}
