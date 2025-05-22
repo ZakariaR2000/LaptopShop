1472c812911abb089f37a916578f4f6295c6a501
ZakariaR2000 <162996600+ZakariaR2000@users.noreply.github.com>
2025-05-22 15:17:30 +0300

    Delete LaptopShop/ApiControllers directory

--- LaptopShop/ApiControllers/Areas/admin/Views/Shared/clsGlobal.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LapShop.Areas.admin.Views.Shared
-{
-    public class clsGlobal
-    {
-        public enum enMode { Add = 1, Update = 2 }
-        public enMode Mode = enMode.Add;
-    }
-}
