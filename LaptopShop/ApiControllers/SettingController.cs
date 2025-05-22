72e61416297836aeeb9a5fe1b458dc9fddef9aeb
ZakariaR2000 <162996600+ZakariaR2000@users.noreply.github.com>
2025-05-22 15:18:57 +0300

    Add files via upload

---
+++ LaptopShop/ApiControllers/SettingController.cs
@@ -0,0 +1,52 @@
+﻿using LapShop.Bl;
+using LapShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LapShop.ApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SettingController : ControllerBase
+    {
+
+        ISetting oClsSettings;
+        public SettingController(ISetting oISettings)
+        {
+            oClsSettings = oISettings;
+        }
+        // GET: api/<SettingController>
+        [HttpGet]
+        public TbSettings Get()
+        {
+            var setting = oClsSettings.GetAll();
+            return setting;
+        }
+
+        // GET api/<SettingController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<SettingController>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<SettingController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<SettingController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ LaptopShop/Areas/admin/Views/Shared/clsGlobal.cs
@@ -0,0 +1,8 @@
+﻿namespace LapShop.Areas.admin.Views.Shared
+{
+    public class clsGlobal
+    {
+        public enum enMode { Add = 1, Update = 2 }
+        public enMode Mode = enMode.Add;
+    }
+}
